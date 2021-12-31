252c940208a190883857d3dd63df315f46f62afb
森林 <gougewh@sina.com>
2021-12-31 11:15:03 +0800

    设置@别名

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -23,5 +24,10 @@ export default defineConfig({
         },
       },
     },
-  }
+  },
+  resolve: {
+    alias: [
+      { find: /^@\//, replacement: `${path.resolve(__dirname, "./src")}/` },
+    ],
+  },
 })
