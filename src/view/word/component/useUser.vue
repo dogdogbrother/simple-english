eca333cf58d550f9ccfd1bfbab3b47a5a40fa6f8
森林 <gougewh@sina.com>
2022-02-25 09:48:05 +0800

    持续开发中

--- src/view/word/component/useUser.vue
+++ src/view/word/component/useUser.vue
@@ -1,12 +1,16 @@
 <template>
-  <div class="use-user" flex-s m-b-20>
-    <h4>正在学习的朋友们:</h4>
-    <div>
-      <Avatar
-        v-for="user in users" 
-        :key="user.id"
-        :src="user.avatar"/>
+  <div style="padding-bottom: 15px;">
+    <span v-if="loading">我倒是看看有多少在学习~  - -!</span>
+    <div flex-s v-else-if="users.length">
+      <h4 style="margin: 0 10px 0 0;">正在学习的朋友们:</h4>
+      <div>
+        <Avatar
+          v-for="user in users" 
+          :key="user.id"
+          :src="user.avatar"/>
+      </div>
     </div>
+    <span v-else>挺可惜,还没人学...</span>
   </div>
 </template>
 
@@ -19,14 +23,14 @@ import { Avatar } from 'ant-design-vue'
 
 const { noteId } = useRoute().params
 const users = ref<userInfoType[]>([])
+const loading = ref(false)
 
+loading.value = true
 getWordUseUser(noteId as string).then((res: any) => {
   users.value = res
-})
+}).finally(() => loading.value = false)
+
 </script>
 
 <style lang="scss" scoped>
-h4 {
-  margin: 0 20px 0 0;
-}
 </style>
